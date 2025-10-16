37c60efc96dc28639bb9e1a0751ee8c5f812ad93
Vofe09 <nazariikovalev@gmail.com>
2025-10-16 23:25:59 +0500

    0.6

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 // server.js
 
-
+const express = require('express');
 const app = express();
 const path = require('path');
-const getPhotos = require('./backend/get-photos');
 
 app.use(express.static('my wn website'));
-app.use('/api/photos', getPhotos);
 
 const PORT = 3000;
 app.listen(PORT, () => {
